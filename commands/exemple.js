008d957d07d0fce137186bd92df01ce540f46af8
Antoine DABIN <54556960+A-DBN@users.noreply.github.com>
2022-06-08 11:53:08 +0200

    Update and rename test.js to exemple.js

--- commands/exemple.js
+++ commands/exemple.js
@@ -4,9 +4,9 @@ const env = require('dotenv').config()
 
 module.exports = {
     data: new SlashCommandBuilder()
-            .setName('test')
-            .setDescription('Makes API call to apex')
-            .addStringOption(option => option.setName('option').setRequired(true).setDescription('stats/game/news')),
+            .setName('Exemple')
+            .setDescription('Command exemple')
+            .addStringOption(option => option.setName('option').setRequired(true).setDescription('Send user message')),
         execute: async (interaction) => {
             const username = interaction.user.username
             const option = interaction.options.getString('option')
