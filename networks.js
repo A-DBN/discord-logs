0e6d18f737aebaa15fbc13114a50770b3a3233b4
Antoine Dabin <antoine.dabin@epitech.eu>
2023-05-12 11:00:58 +0900

    [+] Adding Spotify

--- networks.js
+++ networks.js
@@ -10,6 +10,83 @@ const puppeteer = require('puppeteer');
 
 const idsFilePath = './Stockage/ids.json';
 
+async function updateSpotifyInfo() {
+  // const clientId = process.env.SPOTIFY_CLIENT_ID;
+  // const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+  // const accessTokenEndpoint = 'https://accounts.spotify.com/api/token';
+  // const userProfileEndpoint = 'https://api.spotify.com/v1/me';
+  // const username = 'ZenkiuD';
+  
+  // // Generate access token
+  // const data = await axios.post(accessTokenEndpoint, 'grant_type=client_credentials&scope=user-read-email%20user-read-private', {
+  //   headers: {
+  //     'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   }
+  // });
+  // console.log(data.data)
+  
+  // const access_token = data.data.access_token;
+  // console.log(userProfileEndpoint)
+  // try {
+  //   // Get user profile information for the given username
+  //   console.log(access_token)
+  //   const { data } = await axios.get(userProfileEndpoint, {
+  //     headers: {
+  //       'Authorization': `Bearer ${access_token}`
+  //     }
+  //   });
+  try {
+    const URL = "https://open.spotify.com/user/izbhsh9rxqf0darc2stcap265"
+    const browser = await puppeteer.launch({headless: "new", executablePath: '/usr/bin/chromium-browser'});
+    const page = await browser.newPage();
+
+    await page.goto(URL, {waitUntil: 'networkidle2'});
+    await page.waitForSelector('#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw.lPapCDz3v_LipgXwe8gi > div.main-view-container > div.os-host.os-host-foreign.os-theme-spotify.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.main-view-container__scroll-node.os-host-transition.os-host-overflow.os-host-overflow-y > div.os-padding > div > div > div.main-view-container__scroll-node-child > main > section > div > div.contentSpacing.NXiYChVp4Oydfxd7rT5r.RMDSGDMFrx8eXHpFphqG > div.RP2rRchy4i8TIp1CTmb7 > span.rEN7ncpaUeSGL9z0NGQR > h1')
+    const displayName = await page.$eval('#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw.lPapCDz3v_LipgXwe8gi > div.main-view-container > div.os-host.os-host-foreign.os-theme-spotify.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.main-view-container__scroll-node.os-host-transition.os-host-overflow.os-host-overflow-y > div.os-padding > div > div > div.main-view-container__scroll-node-child > main > section > div > div.contentSpacing.NXiYChVp4Oydfxd7rT5r.RMDSGDMFrx8eXHpFphqG > div.RP2rRchy4i8TIp1CTmb7 > span.rEN7ncpaUeSGL9z0NGQR > h1', el => el.innerText);
+    const followers = await page.$eval('#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw.lPapCDz3v_LipgXwe8gi > div.main-view-container > div.os-host.os-host-foreign.os-theme-spotify.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.main-view-container__scroll-node.os-host-transition.os-host-overflow.os-host-overflow-y > div.os-padding > div > div > div.main-view-container__scroll-node-child > main > section > div > div.contentSpacing.NXiYChVp4Oydfxd7rT5r.RMDSGDMFrx8eXHpFphqG > div.RP2rRchy4i8TIp1CTmb7 > div > span:nth-child(2) > a', el => el.innerText);
+    const profile_picture = await page.$eval('#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw.lPapCDz3v_LipgXwe8gi > div.main-view-container > div.os-host.os-host-foreign.os-theme-spotify.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.main-view-container__scroll-node.os-host-transition.os-host-overflow.os-host-overflow-y > div.os-padding > div > div > div.main-view-container__scroll-node-child > main > section > div > div.contentSpacing.NXiYChVp4Oydfxd7rT5r.RMDSGDMFrx8eXHpFphqG > div.rMpf7sfaPDcj387_52fA > div > div > img', el => el.src);
+    const public = await page.$eval('#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw.lPapCDz3v_LipgXwe8gi > div.main-view-container > div.os-host.os-host-foreign.os-theme-spotify.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.main-view-container__scroll-node.os-host-transition.os-host-overflow.os-host-overflow-y > div.os-padding > div > div > div.main-view-container__scroll-node-child > main > section > div > div.contentSpacing.NXiYChVp4Oydfxd7rT5r.RMDSGDMFrx8eXHpFphqG > div.RP2rRchy4i8TIp1CTmb7 > div > span:nth-child(1)', el => el.innerText);
+    await browser.close();
+
+    const embed = new MessageEmbed()
+      .setColor('#1DB954')
+      .setAuthor(`${displayName}`, profile_picture, URL)
+      .addFields(
+        {name: "Followers", value: `${followers.split(/\s+/)[0]}`, inline: true},
+        {name: "Public Playlists", value: `${public.split(/\s+/)[0]}`, inline: true}
+      )
+      .setThumbnail(profile_picture)
+      .setFooter(`@${displayName} on Spotify`, 'https://i.imgur.com/qvdqtsc.png');
+  
+    let ids = {channelId: null, spotifyEmbedId: null};
+    try {
+      const idsFile = fs.readFileSync(idsFilePath);
+      ids = JSON.parse(idsFile);
+    } catch (error) {
+      console.error(`Error while reading IDs file: ${error}`);
+    }
+  
+    const channel = client.channels.cache.get(ids.channelId);
+    if (!channel) {
+      console.error(`Channel with ID ${ids.channelId} not found.`);
+      return;
+    }
+  
+    if (!ids.spotifyEmbedId) {
+      const message = await channel.send({embeds: [embed]});
+      ids.spotifyEmbedId = message.id;
+      fs.writeFileSync(idsFilePath, JSON.stringify(ids, null, 2));
+    } else {
+      channel.messages.fetch(ids.spotifyEmbedId)
+        .then(message => message.edit({embeds: [embed]}))
+        .catch(error => console.error(`Error while updating Spotify info: ${error}`));
+    }
+  } catch (error) {
+    console.error(`Error while updating Spotify info: ${error}`);
+  }
+}
+
 async function updateTikTokInfo() {
   tiktokUsername = "@areittv"
   try {
@@ -26,7 +103,7 @@ async function updateTikTokInfo() {
   
     const embed = new MessageEmbed()
     .setColor('#EE1D52')
-    .setAuthor(`${username} (${tiktokUsername})`, profileImage)
+    .setAuthor(`${username}`, profileImage, `https://www.tiktok.com/@${tiktokUsername}`)
     .addField('Followers', followers.toLocaleString(), true)
     .addField('Total likes', likes.toLocaleString(), true)
     .setURL(`https://www.tiktok.com/@${tiktokUsername}`)
@@ -103,7 +180,7 @@ async function updateTikTokInfo() {
   
       const embed = new MessageEmbed()
         .setColor('#9146FF')
-        .setTitle(user.display_name)
+        .setAuthor(`${user.display_name}`, user.profile_image_url, 'https://www.twitch.tv/areittv')
         .setDescription(user.description)
         .setThumbnail(user.profile_image_url)
         .setImage(user.offline_image_url)
@@ -267,6 +344,7 @@ module.exports = {
     updateTwitterInfo,
     updateInstagramInfo,
     updateTwitchInfo,
-    updateTikTokInfo
+    updateTikTokInfo,
+    updateSpotifyInfo
 }
 
