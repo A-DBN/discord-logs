0185234ff0e4811e08f706489cded6264d0b69e2
Antoine Dabin <antoine.dabin@epitech.eu>
2023-05-19 19:09:47 +0900

    [+] Adding team.js

---
+++ utils/team.js
@@ -0,0 +1,25 @@
+const {EmbedBuilder} = require('discord.js');
+
+
+function updateEmbed(interaction) {
+    let message = interaction.message;
+    const embed = interaction.message.embeds[0];
+    const components = interaction.message.components;
+
+    const new_embed = new EmbedBuilder()
+        .setColor(embed.color)
+        .setAuthor({name:embed.author.name, iconURL:embed.author.iconURL, url:embed.author.url})
+        .setTitle(embed.title)
+        .setDescription(embed.description)
+        .setTimestamp()
+        .setFooter({text:embed.footer.text})
+    players.forEach(player => {
+        new_embed.addFields({name: `‎`, value: `<@${player}>`, inline: true})
+    })
+
+    message.edit({embeds: [new_embed], components: components})
+}
+
+module.exports = {
+    updateEmbed
+}
