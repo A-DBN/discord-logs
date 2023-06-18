78d26fbdd3b69005ffb85f6661296861cbe45cde
Antoine Dabin <antoine.dabin@epitech.eu>
2023-06-18 22:56:53 +0900

    [+] Add valorant commands

---
+++ commands/riot.js
@@ -0,0 +1,187 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const {EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder} = require('discord.js');
+const axios = require('axios')
+const { createCanvas, loadImage } = require('canvas');
+
+async function createImageGrid(images) {
+    const canvasWidth = 1200;
+    const canvasHeight = 800;
+    const padding = 10;
+    const columns = 1; // Each image on a different line, so only 1 column
+    const rows = images.length; // Number of images determines the number of rows
+    const imageWidth = canvasWidth - padding * 2; // Use full canvas width
+    const imageHeight = (canvasHeight - padding * (rows - 1)) / rows;
+  
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    const context = canvas.getContext('2d');
+  
+    let x = padding; // Start x position with padding
+    let y = padding; // Start y position with padding
+  
+    for (let i = 0; i < images.length; i++) {
+      const image = await loadImage(images[i]);
+      context.drawImage(image, x, y, imageWidth, imageHeight);
+  
+      y += imageHeight + padding; // Increment y position for the next image
+    }
+  
+    return canvas.toBuffer('image/png');
+  }
+  
+
+function setColor() {
+    const color = Math.floor(Math.random() * 0xFFFFFF);
+
+    if (typeof value === 'number') {
+        const modifiedColor = color + value;
+        const finalColor = Math.max(0, Math.min(modifiedColor, 0xFFFFFF));
+        return finalColor.toString(16).padStart(6, '0');
+    }
+    return color.toString(16).padStart(6, '0');
+}
+
+async function getValorantWeapons()
+{
+    let config = {
+        method: 'get',
+        url: 'https://valorant-api.com/v1/weapons',
+        headers: { }
+    };
+
+    return await axios.request(config).then(function (response) {
+        return response.data.data
+    }).catch(function (error) {
+        console.error(error);
+    })
+}
+
+async function global(interaction, data) {
+    await interaction.deferReply();
+    const weapon = interaction.options.getString('nom');
+    const weaponData = data.find(w => w.displayName === weapon);
+    const [wpStats, displayImage, shopData, skins] = [weaponData.weaponStats, weaponData.displayIcon, weaponData.shopData, weaponData.skins];
+
+    const embed = new EmbedBuilder()
+      .setTitle('Weapon Information')
+      .setDescription('Boutton Stats: Stats de l\'arme en jeu\nBoutton Shop: Informations de l\'arme dans le shop\nBoutton Skins: Informations sur les skins')
+      .setImage(displayImage)
+      .setColor(setColor())
+
+    const actionRow = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('stats')
+          .setLabel('Stats')
+          .setStyle(ButtonStyle.Primary),
+        new ButtonBuilder()
+          .setCustomId('shop')
+          .setLabel('Shop')
+          .setStyle(ButtonStyle.Secondary),
+        new ButtonBuilder()
+          .setCustomId('skins')
+          .setLabel('Skins')
+          .setStyle(ButtonStyle.Success)
+      );
+
+    interaction.editReply({ embeds: [embed], components: [actionRow] })
+      .then((message) => {
+        const collector = interaction.channel.createMessageComponentCollector({ time: 60000 });
+
+        collector.on('collect', (buttonInteraction) => {
+            embed.data.fields = [];
+            switch (buttonInteraction.customId) {
+                case 'stats':
+                    const { fireRate, magazineSize, reloadTimeSeconds, wallPenetration, damageRanges } = weaponData.weaponStats;
+                
+                    embed.setDescription('Stats Page');
+                    embed.addFields(
+                    { name: 'Fire Rate', value: fireRate.toString(), inline: true },
+                    { name: 'Magazine Size', value: magazineSize.toString(), inline: true },
+                    { name: 'Reload Time', value: reloadTimeSeconds.toString(), inline: true },
+                    { name: 'Wall Penetration', value: wallPenetration.split(':')[2], inline: true },
+                    );
+                    damageRanges.forEach(range => {
+                    const rangeText = `Range: ${range.rangeStartMeters}-${range.rangeEndMeters}\nHead Damage: ${range.headDamage}\nBody Damage: ${range.bodyDamage}\nLeg Damage: ${range.legDamage}`;
+                    embed.addFields({name:'Damage Range', value:rangeText});
+                    });
+                    break;
+                case 'shop':
+                embed.setDescription('Shop Page');
+                        embed.addFields(
+                            { name: 'Catégorie', value: shopData.category, inline: true },
+                            { name: 'Prix', value: shopData.cost.toString(), inline: true },
+                            { name: `Positon dans la grille`, value: `${shopData.gridPosition.row}-${shopData.gridPosition.column}`, inline: true}
+                        )
+                break;
+                case 'skins':
+                embed.setDescription('Skins Page');
+                embed.addFields(
+                    {
+                        name: 'Skins',
+                        value: skins.map(skin => skin.displayName).join('\n'),
+                    }
+                )
+                break;
+            }
+            buttonInteraction.update({ embeds: [embed]});
+            });
+
+        collector.on('end', () => {
+            actionRow.components.forEach((button) => button.setDisabled(true));
+            interaction.editReply({ components: [actionRow] });
+        });
+    })
+    .catch(console.error);
+}
+
+async function showSkin(interaction, data) {
+    await interaction.deferReply();
+    const name = interaction.options.getString('nom');
+    const skins = data.map(w => w.skins).flat().filter(s => s.displayName === name);
+    const chromas = skins.map(s => s.chromas).flat().slice(1);
+    const images = chromas.map(c => c.fullRender);
+    if (chromas.length === 0 || images.length === 0) return await interaction.editReply({ content: 'Aucun Chroma trouvé. Pour obtenir la liste des skins d\'une arme /valorant arme puis menu Skins' });
+    const image = await createImageGrid(images);
+    const embed = new EmbedBuilder()
+        .setTitle(name)
+        .setImage('attachment://image.png')
+        .setColor(setColor())
+        .setTimestamp()
+        .addFields({ name: 'Chromas in the image order:', value: " ", inline: false})
+    
+    chromas.forEach(chroma => {
+        embed.addFields({ name: chroma.displayName, value: chroma.fullRender, inline: false})
+    })
+    
+    await interaction.editReply({ embeds: [embed], files: [{ attachment: image, name: 'image.png' }] });
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('valorant')
+            .setDescription('Commandes Valorant')
+            .addSubcommand(subcommand => subcommand
+                .setName('arme')
+                .setDescription('Affiche les informations d\'une arme')
+                .addStringOption(option => option.setName('nom').setDescription('Nom de l\'arme').setAutocomplete(true).setRequired(true))
+            )
+            .addSubcommand(subcommand => subcommand
+                .setName('skin')
+                .setDescription('Affiche les informations d\'un skin')
+                .addStringOption(option => option.setName('nom').setDescription('Nom du skin').setRequired(true))
+            ),
+        autocomplete: async (interaction) => {
+            const focusedValue = interaction.options.getFocused(true);
+            const choices = ["Odin", "Ares", 'Vandal', "Bulldog", "Guardian", "Phantom", "Spectre", "Stinger", "Marshal", "Operator", "Shorty", "Frenzy", "Ghost", "Sheriff", "Bucky", "Judge", "Classic", "Knife"];
+            const filtered = choices.filter(choice => choice.startsWith(focusedValue.value));
+            await interaction.respond(filtered.map(choice => ({ name: choice, value: choice })));
+        },
+        execute: async (interaction) => {
+            const data = await getValorantWeapons();
+            if (interaction.options.getSubcommand() === 'arme') {
+                await global(interaction, data);
+            } else if (interaction.options.getSubcommand() === 'skin') {
+                await showSkin(interaction, data);
+            }
+        }
+}
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,88 +4,105 @@ const env = require ('dotenv').config()
 const _ = require('lodash')
 const { updateEmbed } = require('../utils/team.js');
 
+function handleButton(interaction) {
+    console.log("je passe dedans ?")
+    const roleName = interaction.customId;
+    const role = interaction.guild.roles.cache.find(role => role.name === roleName);
+
+    if (role) {
+        const member = interaction.guild.members.cache.get(interaction.user.id);
+        if (interaction.member.roles.cache.has(role.id)) {
+        member.roles.remove(role)
+            .then(() => {
+            console.log(`Removed role ${roleName} from ${member.user.tag}`);
+            })
+            .catch(console.error);
+        } else {
+        member.roles.add(role)
+            .then(() => {
+            console.log(`Assigned role ${roleName} to ${member.user.tag}`);
+            })
+            .catch(console.error);
+        }
+        interaction.deferUpdate()
+    } else {
+        console.log(`Role ${roleName} not found.`);
+    }
+}
+
+async function handleChatInputCommand(interaction) {
+    const command = interaction.client.commands.get(interaction.commandName);
+    
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+}
+
+async function handleFiveStack(interaction)
+{
+    if (interaction.customId === 'Join') {
+        if (players.includes(interaction.user.id)) return 
+        if (players.length > 5) return interaction.reply({ content: 'The team is full!', ephemeral: true })
+        players.push(interaction.user.id);
+        updateEmbed(interaction)
+    } else if (interaction.customId === 'Leave') {
+        const index = players.indexOf(interaction.user.id);
+        if (index > -1) {
+            players.splice(index, 1);
+            updateEmbed(interaction)
+        } else {
+            interaction.reply({ content: 'You are not in the team!', ephemeral: true });
+        }
+    }
+    interaction.deferUpdate()
+}
+
+async function handleAutoComplete(interaction) {
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if(!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try{
+        await command.autocomplete(interaction);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
 module.exports = {
     name: Events.InteractionCreate,
     on: true,
     async execute(interaction) {
         if (interaction.isChatInputCommand()) {
-            const command = interaction.client.commands.get(interaction.commandName);
-    
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-    
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-                } else {
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-                }
-            }
+            await handleChatInputCommand(interaction)
         } else if (interaction.isButton()) {
             if (interaction.channelId === '980471046815772717') {
-                const roleName = interaction.customId;
-                const role = interaction.guild.roles.cache.find(role => role.name === roleName);
-    
-                if (role) {
-                    const member = interaction.guild.members.cache.get(interaction.user.id);
-                    if (interaction.member.roles.cache.has(role.id)) {
-                    member.roles.remove(role)
-                        .then(() => {
-                        console.log(`Removed role ${roleName} from ${member.user.tag}`);
-                        })
-                        .catch(console.error);
-                    } else {
-                    member.roles.add(role)
-                        .then(() => {
-                        console.log(`Assigned role ${roleName} to ${member.user.tag}`);
-                        })
-                        .catch(console.error);
-                    }
-                    interaction.deferUpdate()
-                } else {
-                    console.log(`Role ${roleName} not found.`);
-                }
-            } else if (interaction.isAutocomplete()){
-                console.log("tu veux auto toi ?")
-                const command = interaction.client.commands.get(interaction.commandName);
-
-                if(!command) {
-                    console.error(`No command matching ${interaction.commandName} was found.`);
-                    return;
-                }
-    
-                try{
-                    await command.autocomplete(interaction);
-                } catch(error) {
-                    console.error(error);
-                }
-            }else {
+                handleButton(interaction)
+            } else if (interaction.commandName === 'fivestack') {
                 try {
-                    if (interaction.customId === 'Join') {
-                        if (players.includes(interaction.user.id)) return 
-                        if (players.length > 5) return interaction.reply({ content: 'The team is full!', ephemeral: true })
-                        players.push(interaction.user.id);
-                        updateEmbed(interaction)
-                    } else if (interaction.customId === 'Leave') {
-                        const index = players.indexOf(interaction.user.id);
-                        if (index > -1) {
-                            players.splice(index, 1);
-                            updateEmbed(interaction)
-                        } else {
-                            interaction.reply({ content: 'You are not in the team!', ephemeral: true });
-                        }
-                    }
-                    interaction.deferUpdate()
+                    handleFiveStack(interaction)
                 } catch (error) {
                     interaction.message.delete()
                     interaction.reply({ content: 'Une erreur est survenue, l\'intéraction a donc été supprimée', ephemeral: true });
                 }
             }
+        } else if (interaction.isAutocomplete()){
+            await handleAutoComplete(interaction)
         }
     }
 }
