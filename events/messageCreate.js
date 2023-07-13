7256dae6fc7b91eb606a18520db6e57cbc153590
Antoine Dabin <adabin@hotmail.fr>
2023-07-14 01:39:47 +0200

    [~] Fix event maso

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,8 +8,8 @@ module.exports = {
     if (message.author.bot) return;
     if (message.content.startsWith('!maso')) {
       message.delete()
-      message.channel.send({files: ["../assets/maso.mp4"]})
-    } else if (message.author.id === '655815335257178125' && message.content.contains('maso')) message.reply({files: ["../assets/maso.mp4"]})
+      message.channel.send({files: ["./assets/maso.mp4"]})
+    } else if (message.author.id === '655815335257178125' && message.content.contains('maso')) message.reply({files: ["./assets/maso.mp4"]})
     else if (message.content.toLowerCase().startsWith('!ano') && message.attachments.size > 0) {
       message.delete();
       const title = message.content.split(' ').slice(1).join(' ');
