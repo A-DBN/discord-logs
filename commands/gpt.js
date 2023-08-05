7575633ea2ea6d790868a522d6fa04665933bd2c
Antoine Dabin <adabin@hotmail.fr>
2023-08-05 15:53:50 +0200

    [+] Adding gpt command to bot

---
+++ commands/gpt.js
@@ -0,0 +1,36 @@
+const { Configuration, OpenAIApi } = require("openai");
+const env = require('dotenv').config()
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION_ID,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+
+async function makeGPTRequest(prompt) {
+    try {
+        const res = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            temperature: 0.7,
+            max_tokens: 2048,
+        });
+        return res.data.choices[0].text;
+    } catch (error) {
+        return null
+    }
+  }
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('gpt')
+            .setDescription('Interact with an AI')
+            .addStringOption(option => option.setName('text').setDescription('Ta question ?').setRequired(true)),
+        execute: async (interaction) => {
+            const question = interaction.options.getString('text')
+            const res = await makeGPTRequest(question)
+            if (res === null) return interaction.reply({ content: 'Limite mensuelle atteinte', ephemeral: true })
+            interaction.reply({ content: res })
+          }
+}
